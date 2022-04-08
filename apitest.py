fbfb7aca03ef369226803897d38c139ece210210
benjaminstrandberg <73587010+benjaminstrandberg@users.noreply.github.com>
2022-04-08 14:58:06 +0200

    ojhg

--- apitest.py
+++ apitest.py
@@ -10,5 +10,6 @@ BASE = "http://127.0.0.1:5000/"
 response = requests.get("https://cohesive-photon-346611.ew.r.appspot.com/" + "recipes", {"name":"all"})
 print(response.text)
 
+#https://cohesive-photon-346611.ew.r.appspot.com
 
 #[("Bacon",100),("Eggs",100)]
