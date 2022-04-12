92d2334e313e3250f6859af8f191d88238774c3a
benjaminstrandberg <73587010+benjaminstrandberg@users.noreply.github.com>
2022-04-12 12:02:00 +0200

    ??

--- apitest.py
+++ apitest.py
@@ -1,15 +1,17 @@
 import requests
 
 BASE = "http://127.0.0.1:5000/"
+URL = "https://cohesive-photon-346611.ew.r.appspot.com/recipes"
 
 # response = requests.post(BASE + "recipes", { "name":"BaconAndEggs", "ingredients" : ["Bacon","Egg"],"amount":[100,100]})
 # print(response.json())
 
-# # input()
+ # input()
 
-response = requests.get("https://cohesive-photon-346611.ew.r.appspot.com/" + "recipes", {"name":"all"})
+response = requests.get("https://cohesive-photon-346611.ew.r.appspot.com/recipes", {"name":"Carbonara"})
+#response = requests.get("https://cohesive-photon-346611.ew.r.appspot.com/recipes")
 print(response.text)
 
-#https://cohesive-photon-346611.ew.r.appspot.com
+
 
 #[("Bacon",100),("Eggs",100)]
